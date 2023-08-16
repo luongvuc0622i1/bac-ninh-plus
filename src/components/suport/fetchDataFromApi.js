c1122cc4f8533f23a8df1d062fb0e00598bc4c86
luongvuc0622i1 <vuhienluong.td151218@gmail.com>
2023-08-16 17:00:26 +0700

    ok

--- src/components/suport/fetchDataFromApi.js
+++ src/components/suport/fetchDataFromApi.js
@@ -4,13 +4,4 @@ export async function fetchDataFromApi(YOUR_API_URL) {
     const cutData = fullData.split(' = ')[1].split('\n').filter(line => !line.trim().startsWith('//')).join('\n');
     const jsonData = JSON.parse(cutData);
     return jsonData;
-    // const base64EncodedString = responseData.content;
-    // // Replace with your base64-encoded string
-    // const byteArray = new Uint8Array(
-    //     [...atob(base64EncodedString)].map(char => char.charCodeAt(0))
-    // );
-    // const textDecoder = new TextDecoder();
-    // const valueConverted = textDecoder.decode(byteArray).split('\n').filter(line => !line.trim().startsWith('//')).join('\n').split(' = ')[1];
-    // const decodedString = JSON.parse(valueConverted);
-    // return decodedString;
 }
